2776eae644f577cc8faf7e7fb18fabc350311b82
1613667 <1613667@22hw035077.SOAM.TCS.com>
2020-04-21 15:45:46 -0500

    delete test

--- code-test/src/main/java/com/code/test/model/repository/EntityTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.code.test.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntityTableRepository extends JpaRepository<UserEntityTable, Long>{
-
-}
